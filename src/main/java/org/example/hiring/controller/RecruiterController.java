e0ea44460aab82283a736cab41e98b0e88681cf2
TornovDutta <tornovdutta@gmail.com>
2025-05-11 11:30:32 +0530

    chage the file name

--- src/main/java/org/example/hiring/controller/RecruiterController.java
+++ src/main/java/org/example/hiring/controller/RecruiterController.java
@@ -1,7 +1,7 @@
 package org.example.hiring.controller;
 
 import org.example.hiring.model.Recruiter;
-import org.example.hiring.service.UserService;
+import org.example.hiring.service.RecruiterService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,7 +12,7 @@ import java.util.List;
 @RequestMapping("recruiter")
 public class RecruiterController {
     @Autowired
-    private UserService service;
+    private RecruiterService service;
     @PostMapping("add")
     public ResponseEntity<Recruiter> add(@RequestBody Recruiter user){
         return service.saveUser(user);
--- src/main/java/org/example/hiring/service/RecruiterService.java
+++ src/main/java/org/example/hiring/service/RecruiterService.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 
 @Service
-public class UserService {
+public class RecruiterService {
     @Autowired
     private RecruiterRepo repo;
 
